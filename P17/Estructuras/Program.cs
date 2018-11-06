4a16625d347e7a40288efe86ba102b95fa266547
Carlos <halomau@hotmail.com>
2018-11-05 22:49:06 -0600

    creé doc

---
+++ P17/Estructuras/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estructuras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public struct Marca
+    {
+        public string Nombre;
+        public Tablet [] Tablets;
+        public Celular [] Celulares;
+    }
+
+    public struct Tablet
+    {
+        public string Modelo;
+        public int Año;
+    }
+
+    public struct Celular
+    {
+        public string Modelo;
+        public int Año;
+    }
+}
